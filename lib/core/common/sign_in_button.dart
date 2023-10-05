5b38cce00026b2b0cf589fbdba05bdd9e7625518
Tahmid-kabir-addin <tahmidkabiraddin@gmail.com>
2023-10-06 01:07:17 +0600

    Storing Data to Firestore

---
+++ lib/core/common/sign_in_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit/Theme/pallete.dart';
+import 'package:reddit/core/constants/constants.dart';
+import 'package:reddit/features/auth/controller/auth_controller.dart';
+
+class SignInButton extends ConsumerWidget {
+  const SignInButton({super.key});
+  
+  void signInWithGoogle(WidgetRef ref) {
+    ref.read(authControllerProvider).signInWithGoogle();
+  }
+  
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: ElevatedButton.icon(
+        onPressed: () => signInWithGoogle(ref),
+        icon: Image.asset(
+          Constants.googlePath,
+          width: 35,
+        ),
+        label: const Text(
+          "Continue With Google",
+          style: TextStyle(fontSize: 18, color: Pallete.whiteColor),
+        ),
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(double.infinity, 50),
+        ),
+      ),
+    );
+  }
+}
